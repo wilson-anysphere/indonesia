aae6385d79c6d249b4ed4e2813a6c0b4c502b17f
root <root@ip-10-140-20-127.ec2.internal>
2026-01-10 20:53:16 -0800

    test(types-bridge): add smoke test for generic class loading
    Adds a minimal TypeProvider-backed test to ensure ExternalTypeLoader can load a generic interface and wire its type parameter into method signatures without panicking.

---
+++ crates/nova-types-bridge/tests/loader_smoke.rs
@@ -0,0 +1,62 @@
+use std::collections::HashMap;
+
+use nova_types::{ClassKind, MethodStub, Type, TypeDefStub, TypeEnv, TypeProvider};
+use nova_types_bridge::ExternalTypeLoader;
+
+#[derive(Default)]
+struct MapProvider {
+    stubs: HashMap<String, TypeDefStub>,
+}
+
+impl TypeProvider for MapProvider {
+    fn lookup_type(&self, binary_name: &str) -> Option<TypeDefStub> {
+        self.stubs.get(binary_name).cloned()
+    }
+}
+
+#[test]
+fn loads_generic_class_without_panicking() {
+    let list_stub = TypeDefStub {
+        binary_name: "java.util.List".to_string(),
+        access_flags: 0x0200, // ACC_INTERFACE
+        super_binary_name: Some("java.lang.Object".to_string()),
+        interfaces: vec![],
+        signature: Some("<E:Ljava/lang/Object;>Ljava/lang/Object;".to_string()),
+        fields: vec![],
+        methods: vec![
+            MethodStub {
+                name: "get".to_string(),
+                descriptor: "(I)Ljava/lang/Object;".to_string(),
+                signature: Some("(I)TE;".to_string()),
+                access_flags: 0x0400, // ACC_ABSTRACT
+            },
+            MethodStub {
+                name: "add".to_string(),
+                descriptor: "(Ljava/lang/Object;)Z".to_string(),
+                signature: Some("(TE;)Z".to_string()),
+                access_flags: 0x0400, // ACC_ABSTRACT
+            },
+        ],
+    };
+
+    let mut provider = MapProvider::default();
+    provider
+        .stubs
+        .insert("java.util.List".to_string(), list_stub);
+
+    let mut store = nova_types::TypeStore::default();
+    let mut loader = ExternalTypeLoader::new(&mut store, &provider);
+
+    let list_id = loader
+        .ensure_class("java.util.List")
+        .expect("class should load from provider");
+    let def = store.class(list_id).expect("class def should be present");
+
+    assert_eq!(def.kind, ClassKind::Interface);
+    assert_eq!(def.type_params.len(), 1);
+
+    let e = def.type_params[0];
+    let get = def.methods.iter().find(|m| m.name == "get").unwrap();
+    assert_eq!(get.return_type, Type::TypeVar(e));
+}
+
