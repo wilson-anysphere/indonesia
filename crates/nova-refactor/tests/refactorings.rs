aa3aed210694c5df73e3ff9f9c2c1e85e0862259
Wilson Lin <code@wilsonl.in>
2026-01-12 02:07:19 -0800

    test(refactor): cover inline_variable mid-line decl followed by newline
    Add regression tests for `case 1: int a = ...;\n` (and CRLF) to ensure the inline-variable
    "remove declaration" path:
    - never deletes `case 1:` when the declaration starts mid-line
    - correctly consumes an immediate trailing newline (including `\r\n` as a unit)

--- crates/nova-refactor/tests/refactorings.rs
+++ crates/nova-refactor/tests/refactorings.rs
@@ -2180,10 +2180,77 @@ fn inline_variable_in_switch_one_line_case_label_does_not_delete_case() {
 }
 
 #[test]
-fn inline_variable_in_switch_case_with_declaration_on_own_line_deletes_indent_cleanly() {
+fn inline_variable_in_switch_case_label_declaration_at_eol_deletes_newline() {
+    // Regression test for the mid-line declaration deletion path when the declaration ends the line
+    // (`stmt_end` is immediately followed by a newline). The declaration should be removed without
+    // deleting `case 1:`, and the following statement should be kept.
     let file = FileId::new("Test.java");
     let src = r#"class C {
   void m(int x) {
+    switch (x) {
+      case 1: int a = 1 + 2;
+System.out.println(a); break;
+    }
+  }
+}
+"#;
+
+    let db = RefactorJavaDatabase::new([(file.clone(), src.to_string())]);
+    let offset = src.find("int a").unwrap() + "int ".len();
+    let symbol = db.symbol_at(&file, offset).expect("symbol at a");
+
+    let edit = inline_variable(
+        &db,
+        InlineVariableParams {
+            symbol,
+            inline_all: true,
+            usage_range: None,
+        },
+    )
+    .unwrap();
+    let after = apply_text_edits(src, &edit.text_edits).unwrap();
+
+    let expected = r#"class C {
+  void m(int x) {
+    switch (x) {
+      case 1: System.out.println((1 + 2)); break;
+    }
+  }
+}
+"#;
+    assert_eq!(after, expected);
+}
+
+#[test]
+fn inline_variable_in_switch_case_label_declaration_at_eol_deletes_newline_crlf() {
+    let file = FileId::new("Test.java");
+    let src = "class C {\r\n  void m(int x) {\r\n    switch (x) {\r\n      case 1: int a = 1 + 2;\r\nSystem.out.println(a); break;\r\n    }\r\n  }\r\n}\r\n";
+
+    let db = RefactorJavaDatabase::new([(file.clone(), src.to_string())]);
+    let offset = src.find("int a").unwrap() + "int ".len();
+    let symbol = db.symbol_at(&file, offset).expect("symbol at a");
+
+    let edit = inline_variable(
+        &db,
+        InlineVariableParams {
+            symbol,
+            inline_all: true,
+            usage_range: None,
+        },
+    )
+    .unwrap();
+    let after = apply_text_edits(src, &edit.text_edits).unwrap();
+
+    let expected =
+        "class C {\r\n  void m(int x) {\r\n    switch (x) {\r\n      case 1: System.out.println((1 + 2)); break;\r\n    }\r\n  }\r\n}\r\n";
+    assert_eq!(after, expected);
+}
+
+#[test]
+fn inline_variable_in_switch_case_with_declaration_on_own_line_deletes_indent_cleanly() {
+    let file = FileId::new("Test.java");
+    let src = r#"class C {
+      void m(int x) {
     switch (x) {
       case 1:
         int a = 1 + 2;
